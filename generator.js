b1c3d5e8e20c7cbb79ad799f1ad99ba4b662ea05
Trey Schmieder <98202265+tschmied3@users.noreply.github.com>
2023-12-20 21:48:26 -0600

    Update generator.js

--- generator.js
+++ generator.js
@@ -1,1 +1,23 @@
 
+
+const quotes = [
+"To be the man I know I can be, I must work for it today.", 
+"I do not fear my demons, I thank them for making me stronger.",
+"I am the perfect man.  Through my greater self nothing escapes me.",
+"I am immortal, the energy that composes me has always and will always exist; it just changes forms as will I.",
+"I cannot truly create or destory anything; I can only alter its form.",
+"You do not lose people or let things go, you simply give them back.",
+"You do not choose the landscape your journey begins on, you choose how to traverse it.",
+"Life is a journey where you slowly remember who you are.",
+"Fear and excitement are the same emotion, the same reaction; it is you who determines how you feel.",
+"Live in the moment, you can only live it once.",
+"You can be thankful for anything in life.",
+"Your emotions are you, you are not your emotions.",
+"The goal of everyone should be this: to raise the collective.",
+"",
+"",
+"",
+"",
+"",
+"I did not make the rules, I simply learn how to exploit them."
+];
